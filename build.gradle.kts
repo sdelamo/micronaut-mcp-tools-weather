c07aedd3cdb871b18e4182c52f61cba78c9770cd
Sergio del Amo <sergio.delamo@softamo.com>
2025-09-04 13:46:15 +0200

    remove aot plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("io.micronaut.application") version "4.5.4"
     id("com.gradleup.shadow") version "8.3.7"
-    id("io.micronaut.aot") version "4.5.4"
 }
 
 version = "0.1"
@@ -36,18 +35,6 @@ micronaut {
     processing {
         incremental(true)
         annotations("example.micronaut.*")
-    }
-    aot {
-        // Please review carefully the optimizations enabled below
-        // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
-        optimizeServiceLoading = false
-        convertYamlToJava = false
-        precomputeOperations = true
-        cacheEnvironment = true
-        optimizeClassLoading = true
-        deduceEnvironment = true
-        optimizeNetty = true
-        replaceLogbackXml = true
     }
 }
 
