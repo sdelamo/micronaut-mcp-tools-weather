9b878b15eef068938bf9df8b2b6518852f130f2a
Sergio del Amo <sergio.delamo@softamo.com>
2025-10-07 10:58:12 +0200

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     annotationProcessor("io.micronaut:micronaut-graal")
     annotationProcessor("io.micronaut:micronaut-http-validation")
     implementation("io.micronaut:micronaut-jackson-databind")
-    implementation("io.micronaut.mcp:micronaut-mcp-server-java-sdk:0.0.7")
+    implementation("io.micronaut.mcp:micronaut-mcp-server-java-sdk:0.0.12")
     annotationProcessor("io.micronaut.jsonschema:micronaut-json-schema-processor:1.7.2")
     implementation("io.micronaut.jsonschema:micronaut-json-schema-annotations:1.7.2")
     implementation("io.micronaut:micronaut-http-client-jdk")
